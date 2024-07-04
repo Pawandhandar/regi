28ac90c9ee2ee81a9dcfa6a82ec80f701b3e9fba
Pawandhandar <154113605+Pawandhandar@users.noreply.github.com>
2024-07-04 07:57:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react';
 
 const Register = () => {
   const [name, setName] = useState('');
